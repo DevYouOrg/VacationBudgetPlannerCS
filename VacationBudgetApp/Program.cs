c49769855147a758040bda322736287df5ae939d
Hemory Phifer <hemphife@gmail.com>
2020-04-21 09:24:57 -0400

    ready for class

--- VacationBudgetApp/Program.cs
+++ VacationBudgetApp/Program.cs
@@ -17,74 +17,95 @@ namespace VacationBudgetApp
             Console.WriteLine("What is your name?");
             string userName = Console.ReadLine();
 
-            Console.WriteLine("Welcome " + userName + " !");
+            Console.WriteLine($"Welcome {userName}!");
 
-            Console.WriteLine("Where would you like to travel. Choose: (1)Mexico (2)Jamaica");
-            int locationChoice = Convert.ToInt16(Console.ReadLine());
+            string tryAgain = "y";
 
-            switch (locationChoice)
+            while (tryAgain.ToLower() == "y")
             {
-                case 1:
-                    Console.WriteLine("Great! Mexico sounds like an amazing trip!");
-                    Console.WriteLine("**********");
 
-                    Console.WriteLine("How many days do you plan on spending in Mexico?");
-                    int tripDuration = Convert.ToInt16(Console.ReadLine());
+                Console.WriteLine("Where would you like to travel. Choose: (1)Mexico (2)Jamaica");
+                int locationChoice = Convert.ToInt16(Console.ReadLine());
 
-                    Console.WriteLine("How much money in US dollars do you plan on spending during your trip?");
-                    double money = Convert.ToDouble(Console.ReadLine());
+                switch (locationChoice)
+                {
+                    case 1:
+                        Console.WriteLine("Great! Mexico sounds like an amazing trip!");
+                        Console.WriteLine("**********");
 
-                    double hoursCalculated = tripDuration * 24;
-                    double minutesCalculated = tripDuration * 1440;
+                        Console.WriteLine("How many days do you plan on spending in Mexico?");
+                        int tripDuration = Convert.ToInt16(Console.ReadLine());
 
-                    Console.WriteLine("You will be in Mexico for " + hoursCalculated + " total hours and " +
-                                      minutesCalculated + " total minutes");
+                        Console.WriteLine("How much money in US dollars do you plan on spending during your trip?");
+                        double money = Convert.ToDouble(Console.ReadLine());
 
-                    double budgetInDollars = money / tripDuration;
-                    double pesoConversion = money * 19.23;
-                    double budgetInPesos = pesoConversion / tripDuration;
-
-                    //DecimalFormat df2 = new DecimalFormat("#.##");
-
-                    //Console.WriteLine("During your trip you will have " + df2.format(budgetInDollars) + " USD to spend each day.");
-                    //Console.WriteLine("This equates to " + df2.format(budgetInPesos) + " in Pesos");
-
-                    Console.WriteLine("Thanks for using the Vacation Budget App. Enjoy your vacation!");
+                        double hoursCalculated = tripDuration * 24;
+                        double minutesCalculated = tripDuration * 1440;
 
 
+                        Console.WriteLine($"You will be in Mexico for {hoursCalculated} total hours and {minutesCalculated} in total minutes.");
 
-                    break;
-                case 2:
-                    Console.WriteLine("Great! Jamaica sounds like an amazing trip!");
-                    Console.WriteLine("**********");
 
-                    Console.WriteLine("How many days do you plan on spending in Mexico?");
-                    int jamaicaTripDuration = Convert.ToInt16(Console.ReadLine());
+                        double budgetInDollars = money / tripDuration;
+                        double pesoConversion = money * 19.23;
+                        double budgetInPesos = pesoConversion / tripDuration;
 
-                    Console.WriteLine("How much money in US dollars do you plan on spending during your trip?");
-                    double jamaicanMoney = Convert.ToDouble(Console.ReadLine());
 
-                    double jamaicaHoursCalculated = jamaicaTripDuration * 24;
-                    double jamaicaMinutesCalculated = jamaicaTripDuration * 1440;
+                        Console.WriteLine($"During your trip you will have {budgetInDollars:##} USD to spend each day.");
 
-                    Console.WriteLine("You will be in Mexico for " + jamaicaHoursCalculated + " total hours and " +
-                                      jamaicaMinutesCalculated + " total minutes");
+                        Console.WriteLine($"This equates to {budgetInPesos:##} in Pesos");
 
-                    double jamaicaBudgetInDollars = jamaicanMoney / jamaicaTripDuration;
-                    double jamaicanDollarConversion = jamaicanMoney * 19.23;
-                    double budgetInJamaicanDollars = jamaicanDollarConversion / jamaicaTripDuration;
+                        Console.WriteLine("Thanks for using the Vacation Budget App. Enjoy your vacation!");
 
-                    //DecimalFormat df2 = new DecimalFormat("#.##");
+                        Console.WriteLine("Would you like to try again? Enter y or n");
+                        tryAgain = Console.ReadLine();
 
-                    //Console.WriteLine("During your trip you will have " + df2.format(budgetInDollars) + " USD to spend each day.");
-                    //Console.WriteLine("This equates to " + df2.format(budgetInPesos) + " in Pesos");
 
-                    Console.WriteLine("Thanks for using the Vacation Budget App. Enjoy your vacation!");
-                    break;
-                default:
-                    Console.WriteLine("Not valid");
-                    break;
+
+                        break;
+                    case 2:
+                        Console.WriteLine("Great! Jamaica sounds like an amazing trip!");
+                        Console.WriteLine("**********");
+
+                        Console.WriteLine("How many days do you plan on spending in Jamaica?");
+                        int jamaicaTripDuration = Convert.ToInt16(Console.ReadLine());
+
+                        Console.WriteLine("How much money in US dollars do you plan on spending during your trip?");
+                        double jamaicanMoney = Convert.ToDouble(Console.ReadLine());
+
+                        double jamaicaHoursCalculated = jamaicaTripDuration * 24;
+                        double jamaicaMinutesCalculated = jamaicaTripDuration * 1440;
+
+                        Console.WriteLine($"You will be in Jamaica for {jamaicaMinutesCalculated} total hours and {jamaicaMinutesCalculated} in total minutes.");
+
+                        double jamaicaBudgetInDollars = jamaicanMoney / jamaicaTripDuration;
+                        double jamaicanDollarConversion = jamaicanMoney * 139.71;
+                        double budgetInJamaicanDollars = jamaicanDollarConversion / jamaicaTripDuration;
+
+                        Console.WriteLine($"During your trip you will have {jamaicaBudgetInDollars:##} USD to spend each day.");
+
+                        Console.WriteLine($"This equates to {budgetInJamaicanDollars:##} in Jamaican Dollars");
+
+                        Console.WriteLine("Thanks for using the Vacation Budget App. Enjoy your vacation!");
+
+                        Console.WriteLine("Would you like to try again? Enter y or n");
+                        tryAgain = Console.ReadLine();
+
+                        break;
+                    default:
+                        Console.WriteLine("Please select a valid option.");
+
+                        Console.WriteLine("Would you like to try again? Enter y or n");
+                        tryAgain = Console.ReadLine();
+
+                        break;
+
+                } 
             }
+
+
+            Console.WriteLine("Press ENTER to exit app");
+            Console.ReadLine();
         }
     }
 }
